3c8b6b4110ca35f76f411766e7034f4b51c8e405
fidasalim <42141530+fidasalim@users.noreply.github.com>
2018-09-06 15:02:10 -0400

    Add files via upload

---
+++ lab5.5_q11.cpp
@@ -0,0 +1,15 @@
+//include library
+#include <iostream>
+using namespace std;
+
+int main(){// Looping for making patterns
+	//printing stars in inverted right angled 
+	for(int i =0; i<5; i++){
+		//printing space
+		int j =0;
+		while(j < 5-i){
+		cout<<"*";
+		j++;}
+cout<<endl;}
+return 0;
+} 
